96045f25b07065f372b2ee3fc7273c86b7277821
tmadorahmed <tmadorahmed88@gmail.com>
2023-11-22 14:30:07 -0800

    4 5 6

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""that Define a class Square."""
+
+
+class Square:
+    """that Represent a square."""
+
+    def __init__(self, size):
+        """that Initialize a new square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """that Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """that Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """that Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
