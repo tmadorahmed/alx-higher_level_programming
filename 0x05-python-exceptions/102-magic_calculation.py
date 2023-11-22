f53b370facd1b10ba122218c3c06cfbd3f32c571
tmadorahmed <tmadorahmed88@gmail.com>
2023-11-22 13:39:19 -0800

    7 8 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception('Too far')
+            result += a ** b / x
+        except Exception:
+            result = b + a
+            break
+    return result
